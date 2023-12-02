cef6e645627a663aea15479697cef4799518041c
Ruthie Lawriana <lawryruth235@gmail.com>
2023-12-02 14:06:49 +0300

    initial commit

---
+++ Login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ body{ 
+ 	background-image: url('pup.jpg');
+ 	background-size: cover;
+ 	background-position: center center;
+ 	background-repeat: no-repeat;
+ 	height: 100vh}
+</style>
+</head>
+<body>
+	<h1 style=" color: black; font-size: 36xp; font-style:italic; text-align: center;"> WOOFS </h1>
+</body>
+</html>
