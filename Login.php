29ef8762c2fa0df8ab46fc83af501a888a9c3a25
Ruthie Lawriana <lawryruth235@gmail.com>
2023-12-13 12:02:34 +0300

    Second commit

--- Login.php
+++ Login.php
@@ -1,4 +1,39 @@
-<!DOCTYPE html>
+<?php
+require 'connect.php';
+if (isset($_POST['submit'])) {
+	$username=$_POST['username']
+	$password=$_POST['password']
+
+	if (empty($name)|| empty($password)) {
+		$error="Please enter both username and password"; 
+	} else {
+		$sql="SELECT u_id,username,password FROM woofsdata WHERE username= ?";
+		$stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 1) {
+            $user = $result->fetch_assoc();
+
+            if (password_verify($password, $user['password'])) {
+            	$_SESSION['userId']=$user['u_id'];
+            	$_SESSION['username']=$user['username'];
+            	header("Location: home.html");
+
+            	exit();
+            	
+            } else {
+            	$error="Invalid password.";
+            } else {
+            	$error="Invalid username.";
+
+            }
+            $stmt->close();
+	}
+}
+	
+?>
 <html>
 <head>
 <style>
@@ -11,6 +46,25 @@
 </style>
 </head>
 <body>
-	<h1 style=" color: black; font-size: 36xp; font-style:italic; text-align: center;"> WOOFS </h1>
+	<h1 style=" color: black; font-size: 36px; font-style:italic; text-align: center;"> WOOFS </h1>
+	<h2 style="font-size: 24px; font-style:italic; text-align: center">Login</h2>
+	<?php
+	if (isset($error)) {
+		echo '<p style= "color: red;">'.$error.'</p>';	 	
+	 } 
+	?>
+
+	<form method="post" action="login.php">
+		<label for="username">Username:</label>
+		<input type="text" name="username" required>
+		<label for="password"> Password:</label>
+		<input type="password" name="password">
+
+		<button type="submit">Login</button>
+
+	</form>
+
+	<p>Don't have an account, <a href="register.php"> Register here</a></p>
+
 </body>
 </html>
